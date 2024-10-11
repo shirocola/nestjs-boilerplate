54fae0d6d5ef91a77ca32d7020ee0ae646c49328
shirocola <kitpivatpiromgit@gmail.com>
2024-10-11 22:42:26 +0700

    Update app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
+// app.module.ts
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import ormconfig from './ormconfig';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot(ormconfig),
+    // Other modules...
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
